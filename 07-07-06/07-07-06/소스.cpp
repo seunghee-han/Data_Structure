85995ec636de52cb886739fe08ee64cc4a344319
SeungHee Han <54052702+seunghee-han@users.noreply.github.com>
2020-06-08 14:56:55 +0900

    Update 소스.cpp

--- 07-07-06/07-07-06/소스.cpp
+++ 07-07-06/07-07-06/소스.cpp
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 #define MAX_ELEMENT 100
 
-//히프에 대한 1차원 배열과 히프 원소의 개수를 구조체로 묶어서 선언
 typedef struct {
 	int heap[MAX_ELEMENT];
 	int heap_size;
 }heapType;
 
-//공백 히프를 생성하는 연산
+
 heapType* createHeap() {
 	heapType* h = (heapType*)malloc(sizeof(heapType));
 	h->heap_size = 0;
 	return h;
 }
 
-//히프에 item을 삽입하는 연산
+
 void insertHeap(heapType* h, int item) {
 	int i; h->heap_size = h->heap_size + 1;
 	i = h->heap_size;
@@ -26,7 +25,7 @@ void insertHeap(heapType* h, int item) {
 	h->heap[i] = item;
 }
 
-//히프의 루트를 삭제하여 반환하는 연산
+
 int deleteHeap(heapType* h) {
 	int parent, child;
 	int item, temp;
@@ -49,7 +48,6 @@ int deleteHeap(heapType* h) {
 	return item;
 }
 
-//1차원 배열 히프의 내용을 출력하는 연산
 void printHeap(heapType* h) {
 	int i;
 	printf("Heap : ");
