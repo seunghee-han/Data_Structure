effd854749fdb60c64f1e9457757a0b2c0a9835b
승희 한 <hee11273@cu.ac.kr>
2020-06-22 18:43:32 +0900

    ch08-08-03

--- ch080803/ch080803/소스.cpp
+++ ch080803/ch080803/소스.cpp
@@ -1,6 +1,91 @@
-;
-node->link = g->adjList_H[u];
-g->adjList_H[u] = node;
+#include<stdio.h>
+#include<memory.h>
+#include <stdlib.h>
+#define MAX_VERTEX 10
+#define FALSE 0
+#define TRUE 1
+
+typedef struct graphNode {
+	int vertex;
+	struct graphNode* link;
+}graphNode;
+
+typedef struct graphType {
+	int n;
+	graphNode* adjList_H[MAX_VERTEX];
+	int visited[MAX_VERTEX];
+}graphType;
+
+
+typedef int element;
+
+typedef struct stackNode {
+	int data;
+	struct stackNode* link;
+}stackNode;
+
+stackNode* top;
+
+int isEmpty() {
+	if (top == NULL)return 1;
+	else return 0;
+}
+
+void push(int item) {
+	stackNode* temp = (stackNode*)malloc(sizeof(stackNode));
+	temp->data = item;
+	temp->link = top;
+	top = temp;
+}
+
+int pop() {
+	int item;
+	stackNode* temp = top;
+
+	if (isEmpty()) {
+		printf("\n\n Stack is empty ! \n");
+		return 0;
+	}
+	else {
+		item = temp->data;
+		top = temp->link;
+		free(temp);
+		return item;
+	}
+}
+
+
+void createGraph(graphType* g) {
+	int v;
+	g->n = 0;
+	for (v = 0; v < MAX_VERTEX; v++) {
+		g->visited[v] = FALSE;
+		g->adjList_H[v] = NULL;
+	}
+}
+
+
+void insertVertex(graphType* g, int v) {
+	if (((g->n) + 1) > MAX_VERTEX) {
+		printf("\n 그래프 정점의 개수를 초과하였습니다!");
+		return;
+	}
+	g->n++;
+}
+
+
+void insertEdge(graphType* g, int u, int v) {
+	graphNode* node;
+
+
+	if (u >= g->n || v >= g->n) {
+		printf("\n 그래프에 없는 정점입니다.");
+		return;
+	}
+	node = (graphNode*)malloc(sizeof(graphNode));
+	node->vertex = v;
+	node->link = g->adjList_H[u];
+	g->adjList_H[u] = node;
 }
 
 
